d048556788cea4aa9c615e06c135009d18771b90
Richard Hpa <richard.hpa@hotmail.com>
2020-03-09 16:39:38 +1300

    cleaned up some code

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {
-    Container,
     ListGroup,
     ListGroupItem,
     Button
@@ -31,29 +30,27 @@ class ShoppingList extends Component{
         // this.props.item.items
         const { items } = this.props.item;
         return(
-            <Container>
-                <ListGroup>
-                    <TransitionGroup className="shopping-list">
-                    {items.map(({_id, name}) => (
-                        <CSSTransition key={_id} timeout={500} classNames="fade">
-                            <ListGroupItem>
-                                {
-                                    this.props.isAuthenticated ?
+            <ListGroup>
+                <TransitionGroup className="shopping-list">
+                {items.map(({_id, name}) => (
+                    <CSSTransition key={_id} timeout={500} classNames="fade">
+                        <ListGroupItem>
+                            {
+                                this.props.isAuthenticated ?
 
-                                <Button
-                                    className="remove-btn"
-                                    color="danger"
-                                    size="sm"
-                                    onClick={this.onDeleteClick.bind(this, _id)}
-                                >&times;</Button> : null
-                            }
-                                 {name}
-                            </ListGroupItem>
-                        </CSSTransition>
-                    ))}
-                    </TransitionGroup>
-                </ListGroup>
-            </Container>
+                            <Button
+                                className="remove-btn"
+                                color="danger"
+                                size="sm"
+                                onClick={this.onDeleteClick.bind(this, _id)}
+                            >&times;</Button> : null
+                        }
+                             {name}
+                        </ListGroupItem>
+                    </CSSTransition>
+                ))}
+                </TransitionGroup>
+            </ListGroup>
         )
     }
 }
